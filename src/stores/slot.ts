29edf310c1fbcf3d097a519300de4bc44750654f
Renato Mattos <renatomattos.gwb.tfs@outlook.com>
2022-04-28 23:52:09 +0000

    feat: docker compose added

--- src/stores/slot.ts
+++ src/stores/slot.ts
@@ -2,53 +2,55 @@ import type { SlotType } from '@/types/SlotType';
 import { defineStore } from 'pinia';
 
 export type RootState = {
-  all: SlotType[] | [];
+  // all: SlotType[] | [];
+  all: any[];
 };
 
 export const useSlotStore = defineStore({
   id: 'slot',
   state: () =>
     ({
-      all: [
-        {
-          id: 1,
-          title: 'Ações',
-          subtitle: 'Sua gaveta de ações',
-          percent: 0.3,
-        },
-        {
-          id: 2,
-          title: 'Imoveis',
-          subtitle: 'Sua gaveta de imoveis',
-          percent: 0.5,
-        },
-        {
-          id: 3,
-          title: 'Criptomoeda',
-          subtitle: 'Sua gaveta de criptomoeda',
-          percent: 0.15,
-        },
-        {
-          id: 4,
-          title: 'Renda fixa',
-          subtitle: 'Sua gaveta de renda fixa',
-          percent: 0.05,
-        },
-        {
-          id: 5,
-          title: 'Ações Brasil',
-          subtitle: 'Lorem ipsom',
-          percent: 0.6,
-          parent_id: 1,
-        },
-        {
-          id: 6,
-          title: 'Ações EUA',
-          subtitle: 'Lorem ipsom',
-          percent: 0.4,
-          parent_id: 1,
-        },
-      ],
+      // all: [
+      //   {
+      //     id: 1,
+      //     title: 'Ações',
+      //     subtitle: 'Sua gaveta de ações',
+      //     percent: 0.3,
+      //   },
+      //   {
+      //     id: 2,
+      //     title: 'Imoveis',
+      //     subtitle: 'Sua gaveta de imoveis',
+      //     percent: 0.5,
+      //   },
+      //   {
+      //     id: 3,
+      //     title: 'Criptomoeda',
+      //     subtitle: 'Sua gaveta de criptomoeda',
+      //     percent: 0.15,
+      //   },
+      //   {
+      //     id: 4,
+      //     title: 'Renda fixa',
+      //     subtitle: 'Sua gaveta de renda fixa',
+      //     percent: 0.05,
+      //   },
+      //   {
+      //     id: 5,
+      //     title: 'Ações Brasil',
+      //     subtitle: 'Lorem ipsom',
+      //     percent: 0.6,
+      //     parent_id: 1,
+      //   },
+      //   {
+      //     id: 6,
+      //     title: 'Ações EUA',
+      //     subtitle: 'Lorem ipsom',
+      //     percent: 0.4,
+      //     parent_id: 1,
+      //   },
+      // ],
+      all: [],
     } as RootState),
   getters: {
     getMainSlots: (state) => state.all.filter((item) => !item.parent_id),
@@ -56,5 +58,17 @@ export const useSlotStore = defineStore({
       return state.all.filter((item) => item.parent_id === parent_id);
     },
   },
-  actions: {},
+  actions: {
+    getAllSlots() {
+      try {
+        fetch('http://localhost:8055/items/Slot')
+          .then((response) => response.json())
+          .then((data) => {
+            this.all = data.data;
+          });
+      } catch (error) {
+        return error;
+      }
+    },
+  },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <h4>Total da alocação: {{ totalPercent }}</h4>
     </div>
 
-    <template v-for="slot in slots" :key="slot.title">
+    <template v-for="slot in slotsComp" :key="slot.title">
       <div class="col">
         <CardSlot
           :id="slot.id"
@@ -23,13 +23,18 @@
   import CardSlot from '@/components/CardSlot.vue';
   import type { SlotType } from '@/types/SlotType';
   import { usePercent } from '@/composables/usePercent';
+  import { computed } from '@vue/reactivity';
 
   const slotStore = useSlotStore();
   const percent = usePercent();
 
   const slots = ref<SlotType[] | []>([]);
+  slotStore.getAllSlots();
+
+  const slotsComp = computed(() => {
+    return slotStore.getMainSlots;
+  });
 
   slots.value = slotStore.getMainSlots;
-
   const totalPercent = percent.totalPercent(slots.value);
 </script>
